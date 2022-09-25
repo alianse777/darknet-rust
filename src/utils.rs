bf675fddf2a59f40728514a9081ce135cff14e52
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-09-24 19:00:31 -0700

    removed potential panic in `path_to_cstring`

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,14 @@
+use std::os::unix::ffi::OsStrExt;
 use std::{ffi::CString, path::Path};
 
 #[cfg(unix)]
 pub fn path_to_cstring(path: &Path) -> Option<CString> {
-    use std::os::unix::ffi::OsStrExt;
-    Some(CString::new(path.as_os_str().as_bytes()).unwrap())
+    CString::new(path.as_os_str().as_bytes()).ok()
 }
 
 #[cfg(not(unix))]
 pub fn path_to_cstring(path: &Path) -> Option<CString> {
-    path.to_str().map(|s| CString::new(s.as_bytes()).unwrap())
+    path.to_str()
+        .map(|s| CString::new(s.as_bytes()).ok())
+        .flatten()
 }
