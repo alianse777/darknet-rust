4a21598c791cfd86b14f58c6439a205389c3a9e8
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-09-24 20:37:42 -0700

    added path_to_cstring_or_error

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,16 @@
 use std::os::unix::ffi::OsStrExt;
 use std::{ffi::CString, path::Path};
+use crate::Error;
 
 #[cfg(unix)]
 pub fn path_to_cstring(path: &Path) -> Option<CString> {
     CString::new(path.as_os_str().as_bytes()).ok()
+}
+
+pub fn path_to_cstring_or_error(path: &Path) -> Result<CString, Error> {
+    path_to_cstring(path).ok_or_else(|| Error::EncodingError {
+        reason: format!("the path {:?} is invalid", path),
+    })
 }
 
 #[cfg(not(unix))]
